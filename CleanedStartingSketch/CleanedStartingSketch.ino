e609a5ee4949d65fa5846e29bcc0f9fc18f63783
Kris Rau <krau612@gmail.com>
2016-11-11 15:13:45 -0800

    Added function and description for each step

--- CleanedStartingSketch/CleanedStartingSketch.ino
+++ CleanedStartingSketch/CleanedStartingSketch.ino
@@ -1,3 +1,10 @@
+#include <LiquidCrystal_I2C.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
 /*
  This sketch incorperates a constant temperature reading 
  by a onewire Temperature probe during the "normal" running state
@@ -12,7 +19,7 @@
  to be pressed. In order to achieve this, use while loops that only end
  when the desired action in complete. Try to avoid delay functions, and 
  yes I know there are still some being used
-*/
+ */
 
 const int ONE_WIRE_BUS = 53;  // Data wire is plugged into 53 on the Arduino
 const int BACKLIGHT_PIN = 13; // Backlight plugged into 13 on the Arduino
@@ -35,13 +42,22 @@ const int TEST_LED = 13;
 // Keypad constants
 const byte ROWS = 4;
 const byte COLS = 4;
-byte ROW_PINS[ROWS] = { 9, 8, 7, 6 };
-byte COL_PINS[COLS] = { 5, 4, 3, 2 };
+byte ROW_PINS[ROWS] = { 
+  9, 8, 7, 6 };
+byte COL_PINS[COLS] = { 
+  5, 4, 3, 2 };
 const char keymap[ROWS][COLS] = {
-  { 1,   2, 3,   'A' },
-  { 4,   5, 6,   'B' },
-  { 7,   8, 9,   'C' },
-  { '*', 0, '#', 'D' }
+  { 
+    '1', '2', '3', 'A'}
+  ,
+  { 
+    '4', '5', '6', 'B'}
+  ,
+  { 
+    '7', '8', '9', 'C'}
+  ,
+  { 
+    '*', '0', '#', 'D'}
 };
 
 
@@ -71,7 +87,7 @@ void setup () {
   // Initialize serial connection
   Serial.begin(9600);
   sensors.begin();
-  
+
   // Set all relay pins to high because high = open
   for (int i = 31; i <= 45; i += 2) {
     pinMode(i, OUTPUT);
@@ -94,17 +110,17 @@ void loop () {
   // Set cursor to top left
   lcd.setCursor(0, 0);
   lcd.print("Normal Mode");
-  
+
   // Scans for pressed key
   char key = keypad.getKey();
-  
+
   // Update temperatures and display them
   sensors.requestTemperatures();
   lcd.setCursor(0, 1);
   lcd.print("Temp: ");
   temp = sensors.getTempCByIndex(0);
   lcd.print(temp, 2);
-  
+
   // Blink the test led
   digitalWrite(TEST_LED, HIGH);
   delay(50);
@@ -115,32 +131,33 @@ void loop () {
  This function is called whenever a key is pressed.
  Note: this function will stop the main loop code
  until it is complete.
-*/
+ */
 void keypadEvent (KeypadEvent key) {
   if (keypad.getState() == PRESSED) {
     switch (key) {
-      case '*':
-        lcd.clear();
-        ovMode = true;
-        Serial.print(ovMode);
-        lcd.print("Override Mode");
-        delay(1000);
-        
-        while (ovMode) {
-          relayfunc();
-        }
-        
-        break;
-      case '#':
-        lcd.clear();
-        ovMode = false;
-        lcd.print("Normal Mode");
-        delay(1000);
-        break;
-      default:
-        break;
+    case '*':
+      lcd.clear();
+      ovMode = true;
+      Serial.print(ovMode);
+      lcd.print("Override Mode");
+      delay(1000);
+
+      while (ovMode) {
+        relayfunc();
+      }
+
+      break;
+    case '#':
+      lcd.clear();
+      ovMode = false;
+      lcd.print("Normal Mode");
+      delay(1000);
+      break;
+    default:
+      break;
     }
-  } else {
+  } 
+  else {
   }
 }
 
@@ -148,7 +165,7 @@ void relayfunc () {
   lcd.clear();
   lcd.print("Relay Num?: ");
   boolean noKey = true;
-  
+
   // Loop until relay number is chosen
   while (noKey && ovMode) {
     int key = keypad.getKey();
@@ -159,11 +176,11 @@ void relayfunc () {
       noKey = false;
     }
   }
-  
-  
+
+
   lcd.print("Open or Close?: ");
   noKey = true;
-  
+
   // Loop until choice is made to open/close
   while (noKey && ovMode) {
     char key = keypad.getKey();
@@ -173,57 +190,167 @@ void relayfunc () {
       noKey = false;
     }
   }
-  
-  
+
+
   // Match relay number to pin on arduino
   switch (serialNum) {
-    case 1:
-      relayNum = RELAY_1;
-      break;
-    case 2:
-      relayNum = RELAY_2;
-      break;
-    case 3:
-      relayNum = RELAY_3;
-      break;
-    case 4:
-      relayNum = RELAY_4;
-      break;
-    case 5:
-      relayNum = RELAY_5;
-      break;
-    case 6:
-      relayNum = RELAY_6;
-      break;
-    case 7:
-      relayNum = RELAY_7;
-      break;
-    case 8:
-      relayNum = RELAY_8;
-      break;
+  case 1:
+    relayNum = RELAY_1;
+    break;
+  case 2:
+    relayNum = RELAY_2;
+    break;
+  case 3:
+    relayNum = RELAY_3;
+    break;
+  case 4:
+    relayNum = RELAY_4;
+    break;
+  case 5:
+    relayNum = RELAY_5;
+    break;
+  case 6:
+    relayNum = RELAY_6;
+    break;
+  case 7:
+    relayNum = RELAY_7;
+    break;
+  case 8:
+    relayNum = RELAY_8;
+    break;
   }
-  
+
   lcd.clear();
-  
+
   // Open/Close the selected valve
   if (serialOC == 'A' && ovMode) {
     digitalWrite(relayNum, LOW);
-    
+
     lcd.print("Valve ");
     lcd.setCursor(6, 0);
     lcd.print(serialNum);
     lcd.setCursor(8, 0);
     lcd.print(" Opened");
-  } else if (serialOC == 'B' && ovMode) {
+  } 
+  else if (serialOC == 'B' && ovMode) {
     digitalWrite(relayNum, HIGH);
-    
+
     lcd.print("Valve ");
     lcd.setCursor(6, 0);
     lcd.print(serialNum);
     lcd.setCursor(8, 0);
     lcd.print(" Closed");
   }
-  
+
   // Give time for valve to open/close
   delay(1000);
+}
+
+// Grouping
+// Stage #
+// Description
+// I/O
+// End condition
+
+// WVO loading group
+void step1 () {
+  // load VWO through filter
+  // Everything turned off
+  // when loading is finished
+}
+// end WVO loading group
+
+// Prewash group
+void step2 () {
+  // heat oil
+  // turn on bubblers
+  // mist water into washer/drier
+}
+
+void step3 () {
+  // Keep bubblers running
+  // p3 on
+  // time
+}
+
+void step4 () {
+  // wait for wash water to settle
+  // everything turned off
+  // time
+}
+
+void step5 () {
+  // drain wash water
+  // v5 open
+  // until light sensor registers oil instead of water
+}
+
+void step6 () {
+  // dry VWO
+  // p2 on
+  // time
+}
+// end prewash group
+
+// Reaction group
+void step7 () {
+  // transfer VWO into reactor, attach carboy w/Methoxide, ball valve must be closed
+  // v1 open
+  // v3 close
+  // p1 on
+  // when liquid sensor detects no more oil in washer dryer
+}
+
+void step8 () {
+  // heat oil/methoxide
+  // reactor heat on
+  // until oil is about 50C
+}
+
+void step9 () {
+  // mix/reaction
+  // reactor heat on
+  // p1 on
+  // time
+}
+
+void step10 () {
+  // transfer BD/glycerol to washer dryer
+  // v2 close
+  // v4 open
+  // p1 on
+  // when liquid level sensor detects oil has return to washer dryer
+}
+// end reaction group
+
+// start BD Separation/water wash group
+void step11 () {
+  // drain glycerol
+  // v5 open
+  // until light sensor registers BD instead of oil
+}
+
+void step12 () {
+  step2();
+}
+
+void step13 () {
+  step3();
+}
+
+void step14 () {
+  step4();
+}
+
+void step15 () {
+  step5();
+}
+
+void step16 () {
+  step6();
+}
+
+void step17 () {
+  // pump out BD, turn washer dryer ball valve
+  // p2 on
 }
